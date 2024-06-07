dfbd52dbff9fd428cc5eb333a2c2633444d3407e
zoyron <sgrarora3@gmail.com>
2024-06-07 16:58:25 +0530

    commented the search button

--- src/consts.ts
+++ src/consts.ts
@@ -38,16 +38,16 @@ export const LINKS: Links = [
     HREF: "/", 
   },
   { 
-    TEXT: "Work", 
-    HREF: "/work", 
-  },
-  { 
     TEXT: "Blog", 
     HREF: "/blog", 
   },
   { 
     TEXT: "Projects", 
     HREF: "/projects", 
+  },
+  { 
+    TEXT: "Work", 
+    HREF: "/work", 
   },
 ]
 
